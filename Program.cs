5e5940430f550a84c6a835550b2f0e54deb9db26
Natalia Khasanova <natell_2005@mail.ru>
2023-09-20 10:17:36 +0300

    добавила решене задачи 6

--- Program.cs
+++ Program.cs
@@ -13,22 +13,38 @@
 // }
 
 //Задача 4 Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-Console.WriteLine("Введите первое число: ");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число: ");
-int b = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите третье число: ");
-int c = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите первое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите третье число: ");
+// int c = Convert.ToInt32(Console.ReadLine());
 
-if (a > b && a > c)
+// if (a > b && a > c)
+// {
+//     Console.Write("максимальнное число " + a);
+// }
+// if (b > a && b > c)
+// {
+//     Console.Write("максимальное число " + b);
+// }
+// if (c > b && c > a)
+// {
+//     Console.Write("максимальнное число " + c);
+// }
+
+
+//задача 6  Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 0 && number % 2 == 0)
+
 {
-    Console.Write("максимальнное число " + a);
+    Console.Write("число " + number + " является четным ");
 }
-if (b > a && b > c)
+else
 {
-    Console.Write("максимальное число " + b);
-}
-if (c > b && c > a)
-{
-    Console.Write("максимальнное число " + c);
+    Console.WriteLine("число " + number + " не является четным ");
 }
